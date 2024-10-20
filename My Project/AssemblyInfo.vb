efbbf2d0679d71a7c4e8144d6ec0b2f52750dcdf
Luciene Oliveira <Luciene Oliveira@CASA2>
2024-10-20 17:14:31 -0300

    Atualização do framework;

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -1,35 +1,9 @@
 ﻿Imports System
 Imports System.Reflection
 Imports System.Runtime.InteropServices
-
-' General Information about an assembly is controlled through the following 
-' set of attributes. Change these attribute values to modify the information
-' associated with an assembly.
-
-' Review the values of the assembly attributes
-
-<Assembly: AssemblyTitle("Projeto Locadora")> 
-<Assembly: AssemblyDescription("")> 
-<Assembly: AssemblyCompany("GGS*KG")> 
-<Assembly: AssemblyProduct("Projeto Locadora")> 
-<Assembly: AssemblyCopyright("Copyright © GGS*KG 2009")> 
 <Assembly: AssemblyTrademark("")> 
 
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
 <Assembly: Guid("ab0f1d73-7f7e-4aaf-82e7-77a4e02d006b")> 
-
-' Version information for an assembly consists of the following four values:
-'
-'      Major Version
-'      Minor Version 
-'      Build Number
-'      Revision
-'
-' You can specify all the values or you can default the Build and Revision Numbers 
-' by using the '*' as shown below:
-' <Assembly: AssemblyVersion("1.0.*")> 
-
-<Assembly: AssemblyVersion("1.0.0.0")> 
-<Assembly: AssemblyFileVersion("1.0.0.0")> 
